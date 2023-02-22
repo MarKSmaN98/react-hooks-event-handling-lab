194aec5373c7bda6c113e295f14378164385c44e
Mark Coats <alexco1234@gmail.com>
2023-02-22 08:39:19 -0600

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+function EyesOnMe () {
+    function onme() {
+        console.log('Good!');
+    }
+
+    function offme() {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <div name='Eyes'>
+            <button onFocus={onme} onBlur={offme}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
